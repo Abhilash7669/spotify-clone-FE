eba1dff821f8af055653620c5d1e7242a320ddc0
Abhilash7669 <abhilashsk1998@gmail.com>
2025-11-29 00:35:40 +0530

    bringing route guards back up

--- src/router/index.ts
+++ src/router/index.ts
@@ -61,22 +61,22 @@ const router = createRouter({
   ],
 })
 
-// router.beforeEach((to, from, next) => {
-//   const token = AUTH_UTILS.getToken()
+router.beforeEach((to, from, next) => {
+  const token = AUTH_UTILS.getToken()
 
-//   if (to.matched.some((route) => route.meta.requiresAuth)) {
-//     if (token) {
-//       next()
-//     } else {
-//       next('/login')
-//     }
-//   } else {
-//     if (to.path === '/login' && token) {
-//       next('/auth/dashboard')
-//     } else {
-//       next()
-//     }
-//   }
-// })
+  if (to.matched.some((route) => route.meta.requiresAuth)) {
+    if (token) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    if (to.path === '/login' && token) {
+      next('/auth/dashboard')
+    } else {
+      next()
+    }
+  }
+})
 
 export default router
