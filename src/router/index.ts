c8a5dfbc494aa329c61dd04b20ec0e42c2759203
Abhilash7669 <abhilashsk1998@gmail.com>
2025-11-29 00:42:31 +0530

    temporarily modifying route guard to not check for auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -62,21 +62,31 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  const token = AUTH_UTILS.getToken()
+  // const token = AUTH_UTILS.getToken()
 
-  if (to.matched.some((route) => route.meta.requiresAuth)) {
-    if (token) {
-      next()
-    } else {
-      next('/login')
-    }
+  if(to.path === '/') {
+    next("/auth/dashboard")
   } else {
-    if (to.path === '/login' && token) {
-      next('/auth/dashboard')
-    } else {
-      next()
-    }
+    next();
   }
+
+  // if (to.matched.some((route) => route.meta.requiresAuth)) {
+  //   if (token) {
+  //     next()
+  //   } else {
+  //     next('/login')
+  //   }
+  // } else {
+  //   if (to.path === '/login' && token) {
+  //     next('/auth/dashboard')
+  //   }
+  //   else if(to.path === '/' && token) {
+  //     next('/auth/dashboard')
+  //   } 
+  //   else {
+  //     next()
+  //   }
+  // }
 })
 
 export default router
